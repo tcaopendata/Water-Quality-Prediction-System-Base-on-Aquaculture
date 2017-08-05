7f22ae7ac472e4574715d99045a56b1354ce2ca9
tonyliou <30740286+tonyliou@users.noreply.github.com>
2017-08-05 23:24:31 +0800

    Add files via upload

---
+++ Water_Quality_Prediction_System_Base_on_Aquaculture.ino
@@ -0,0 +1,19 @@
+#include "ph.h"
+#include "water_temperature.h"
+#include "bt.h"
+#include <Servo.h>
+Servo myservo;
+
+void setup() {
+  Serial.begin(9600);
+  water_temperature_setup();
+  bt_setup();
+  myservo.attach(9);
+}
+
+void loop() {
+  water_temperature_start();
+  ph_start();
+  bt_send(water_temperature_raw_val, ph_raw_val);
+  myservo.write(180);
+}
---
+++ bt.h
@@ -0,0 +1,25 @@
+#include <SoftwareSerial.h>
+SoftwareSerial bt(10, 11);
+
+void bt_setup() {
+  bt.begin(9600);
+}
+
+void bt_send(float n1, float n2) {  //水溫 pH 混濁 鹽度
+  int data[12];
+  n1 *= 100;
+  n2 *= 100;
+  data[0] = 'a';
+  data[1] = (int)(n1) / 256;
+  data[2] = (int)(n1) % 256;
+  data[3] = 'b';
+  data[4] = (int)(n2) / 256;
+  data[5] = (int)(n2) % 256; 
+
+  byte bt_buffer = bt.read();
+  if (bt_buffer == 49) {
+    for (int j = 0; j < 6; j++) {
+      bt.write(data[j]);
+    }
+  }
+}
